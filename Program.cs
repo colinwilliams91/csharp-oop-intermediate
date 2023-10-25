245ea750eaa151c7c3bc1320903cd788615ffdb1
colin <colin.williams.dev@gmail.com>
2023-10-24 22:38:11 -0500

    feat: complete with exception throws

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using System;
 
 Console.WriteLine($"Stopwatch Assignment Begin Here:");
+
+//Console.WriteLine(Stopwatch.Start());
+
+//Console.WriteLine(Stopwatch.Stop());
+
+//Console.WriteLine(Stopwatch.Start());
+
+//Console.WriteLine(Stopwatch.Stop());
+
+Stopwatch.Start();
+
+Stopwatch.Stop();
--- Stopwatch.cs
+++ Stopwatch.cs
@@ -2,8 +2,38 @@
 
 public class Stopwatch
 {
-	public Stopwatch()
-	{
+    /* Class with Static members */
+	
+    public static DateTime? Time { get; set; }
 
-	}
+	public static TimeSpan ElapsedTime { get; set; }
+
+	public static DateTime Start()
+    {
+		if (Time == null)
+        {
+			Time = DateTime.Now;
+            Console.WriteLine($"The current time is: {Time.Value}");
+			return Time.Value;
+        }
+        else
+        {
+			throw new InvalidOperationException("Stopwatch has already been started.");
+        }
+    }
+
+	public static TimeSpan Stop()
+    {
+        if (Time != null)
+        {
+			ElapsedTime = DateTime.Now - Time.Value;
+			Time = null;
+            Console.WriteLine($"Time elapsed: {ElapsedTime}. Resetting Stopwatch.");
+			return ElapsedTime;
+        }
+        else
+        {
+            throw new InvalidOperationException("Stopwatch has not been started yet.");
+        }
+    }
 }
