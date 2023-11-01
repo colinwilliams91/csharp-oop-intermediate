73b33411d32e16a4fc3a969af5fe4ea99db6bb5e
colin <colin.williams.dev@gmail.com>
2023-11-01 00:34:21 -0500

    feat: implement Class and test all methods passing

--- Program.cs
+++ Program.cs
@@ -13,3 +13,23 @@ Console.WriteLine($"Stopwatch Assignment Begin Here:");
 Stopwatch.Start();
 
 Stopwatch.Stop();
+
+Console.WriteLine("---");
+
+Post one = new Post("My name is Colin", "I am an application developer for the Louisiana Department of Health CDC SHHP.");
+
+Console.WriteLine(one.Title);
+Console.WriteLine(one.Description);
+Console.WriteLine(one.CreatedAt);
+
+one.Upvote();
+Console.WriteLine(one.Votes);
+one.Upvote();
+Console.WriteLine(one.Votes);
+one.Downvote();
+Console.WriteLine(one.Votes);
+one.Upvote();
+Console.WriteLine(one.Votes);
+one.Upvote();
+
+Console.WriteLine(one.Votes);
