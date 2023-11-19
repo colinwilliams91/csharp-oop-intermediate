ce8c63c7b6e4a678f0d389b1d42212a760b3b38f
colin <colin.williams.dev@gmail.com>
2023-11-19 15:18:42 -0600

    feat: simple foreach workflow class arg in static Run method

---
+++ Engine/WorkflowEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_intermediate.Engine
+{
+    public static class WorkflowEngine
+    {
+        public static void Run(Workflow workflow)
+        {
+            foreach (IActivity activity in workflow.Activities)
+            {
+                activity.Execute();
+            }
+        }
+    }
+}
