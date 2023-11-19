b6d29a2db3541702106a8e4d4ca900eb58babca5
colin <colin.williams.dev@gmail.com>
2023-11-19 15:43:21 -0600

    refactor: public prop to GetTasks return enumerable to protect internal

--- Engine/Workflow.cs
+++ Engine/Workflow.cs
@@ -6,18 +6,33 @@ using System.Threading.Tasks;
 
 namespace csharp_oop_intermediate.Engine
 {
-    public class Workflow
+    public class Workflow : IWorkflow
     {
         private List<IActivity> _activities;
 
-        public Workflow(List<IActivity> activities)
+        public Workflow()
         {
-            if (activities == null)
-                throw new ArgumentNullException("Cannot instantiate Workflow. Workflow activities empty.");
+            // if we accept a List<IActivity> as ctor arg we want to check for null
+            //if (activities == null)
+            //    throw new ArgumentNullException("Cannot instantiate Workflow. Workflow activities empty.");
 
-            this.Activities = activities;
+            //this.Activities = activities;
+            this._activities = new List<IActivity>();
         }
 
-        public List<IActivity> Activities { get => _activities; set => _activities = value; }
+        public void Add(IActivity activity)
+        {
+            this._activities.Add(activity);
+        }
+
+        public void Remove(IActivity activity)
+        {
+            this._activities.Remove(activity);
+        }
+
+        public IEnumerable<IActivity> GetTasks()
+        {
+            return this._activities;
+        }
     }
 }
