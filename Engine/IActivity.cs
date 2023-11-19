0f7818f5b3c7ebf9d31f0c680cd155d05244b121
colin <colin.williams.dev@gmail.com>
2023-11-19 15:17:24 -0600

    feat: init IActivity interface, expose execute method contract

---
+++ Engine/IActivity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_intermediate.Engine
+{
+    public interface IActivity
+    {
+        public void Execute();
+    }
+}
