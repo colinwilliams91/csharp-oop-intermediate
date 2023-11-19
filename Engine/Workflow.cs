a2c4706a9a5655079f34b325262aa90d264372ca
colin <colin.williams.dev@gmail.com>
2023-11-19 15:17:59 -0600

    feat: init workflow class List activities Prop

---
+++ Engine/Workflow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_intermediate.Engine
+{
+    public class Workflow
+    {
+        private List<IActivity> _activities;
+
+        public Workflow(List<IActivity> activities)
+        {
+            if (activities == null)
+                throw new ArgumentNullException("Cannot instantiate Workflow. Workflow activities empty.");
+
+            this.Activities = activities;
+        }
+
+        public List<IActivity> Activities { get => _activities; set => _activities = value; }
+    }
+}
