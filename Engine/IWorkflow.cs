867b8a7f192a5bd92dac9d4807de726d3fb4446c
colin <colin.williams.dev@gmail.com>
2023-11-19 15:42:51 -0600

    feat: add interface for Workflow, expose GetTasks enumerable return

---
+++ Engine/IWorkflow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_intermediate.Engine
+{
+    public interface IWorkflow
+    {
+        void Add(IActivity activity);
+        void Remove(IActivity activity);
+        IEnumerable<IActivity> GetTasks();
+    }
+}
